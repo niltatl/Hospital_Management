435060076868690882e9f28406bd83d5a014da32
niltatl <145554133+niltatl@users.noreply.github.com>
2024-08-15 14:53:50 +0300

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+
+
+const Header = () => {
+  return <header className="header flex items-center">
+    <div className="container">
+     <div>
+      
+     </div>
+    </div>
+  </header>
+};
+
+export default Header;
